6a7df34c350c9a07790b58b4ac41222bf8b09678
mettaneko <55776002+mettaneko@users.noreply.github.com>
2023-03-30 09:40:18 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,3 +30,6 @@ wikiqg.addEventListener('click', function () {
 wikiqs.addEventListener('click', function () {
     window.location.href = 'balance';
 })
+wikiqc.addEventListener('click', function () {
+    window.location.href = 'alpha';
+})
